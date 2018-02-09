86c0d7d4a5b111be58a44d590d69503296ce4210
Matthew Berryhill <matthewberryhill@gmail.com>
2018-02-09 12:01:15 +0700

    Implement CLI

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ import(
 	"time"
 
 	"github.com/boltdb/bolt"
+	"flag"
+	"os"
 )
 
 const targetBits = 24
@@ -242,8 +244,83 @@ func (i *BlockchainIterator) Next() *Block {
 	return block
 }
 
+type CLI struct {
+	bc *Blockchain
+}
+
+func (cli *CLI) Run() {
+	cli.validateArgs()
+
+	addBlockCmd := flag.NewFlagSet("addblock", flag.ExitOnError)
+	printChainCmd := flag.NewFlagSet("printchain", flag.ExitOnError)
+
+	addBlockData := addBlockCmd.String("data", "", "Block data")
+
+	switch os.Args[1] {
+	case "addblock":
+		err := addBlockCmd.Parse(os.Args[2:])
+		if err != nil {
+			fmt.Println("addblock argument is incorrect")
+		}
+	case "printchain":
+		err := printChainCmd.Parse(os.Args[2:])
+		if err != nil {
+			fmt.Println("printchain argument is incorrect")
+		}
+	default:
+		cli.printUsage()
+		os.Exit(1)
+	}
+
+	if addBlockCmd.Parsed() {
+		if *addBlockData == "" {
+			addBlockCmd.Usage()
+			os.Exit(1)
+		}
+		cli.addBlock(*addBlockData)
+	}
+
+	if printChainCmd.Parsed() {
+		cli.printChain()
+	}
+}
+
+func (cli *CLI) validateArgs() {
+	// todo: IMPELEMENT
+}
+
+func (cli *CLI) printUsage() {
+	// todo: IMPELEMENT
+}
+
+func (cli *CLI) addBlock(data string) {
+	cli.bc.AddBlock(data)
+	fmt.Println("Success!")
+}
+
+func (cli *CLI) printChain() {
+	bci := cli.bc.Iterator()
+
+	for {
+		block := bci.Next()
+
+		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		pow := NewProofOfWork(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+
+		if len(block.PrevBlockHash) == 0 {
+			break
+		}
+	}
+}
+
 func main() {
-	//bc := Blockchain{tip, db}
+	bc := NewBlockchain()
+	defer bc.db.Close()
 
-
+	cli := CLI{bc}
+	cli.Run()
 }
